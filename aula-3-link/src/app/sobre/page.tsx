81d6fcc7c26407f289798b0a3110584f83823fd4
joao vitor <joaovitor646260@gmail.com>
2026-01-08 22:43:35 -0300

    Aprendemos sobre links

---
+++ aula-3-link/src/app/sobre/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Sobre',
+  description: 'sobre nos' 
+}
+
+export default function Sobre(){
+  return (
+    <h1>Sobre</h1>
+  )
+}
---
+++ aula-3-link/src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Menu(){
+  return(
+    
+        <ul className="menu">
+          <li><Link href="/">home</Link></li>
+          <li><Link href="/sobre" prefetch={false}>Sobre</Link></li>
+          <li><Link href="/contato" prefetch={true}>Contato</Link></li>
+        </ul>
+  
+  )
+}
